99c5f6545710f741eb3189683824ff1bd2446558
siscodr <siscodr@rose-hulman.edu>
2015-04-24 19:52:16 -0400

    Task 3 and 4 complete
    Made a fake Semaphore and called expect calls on it with returns. tests
    passed.

--- CommandClientVisualStudioTest/AdvancedMockTests.cs
+++ CommandClientVisualStudioTest/AdvancedMockTests.cs
@@ -78,21 +78,84 @@ namespace CommandClientVisualStudioTest
             byte[] actual = simpleStream.ToArray();
             byte[] expectedArray = { 0, 0, 0, 0, 9, 0, 0, 0, 49, 50, 55, 46, 48, 46, 48, 46, 49, 2, 0, 0, 0, 10, 0 };
             CollectionAssert.AreEqual(expectedArray, actual);
-            
         }
 
         [TestMethod]
         public void TestSemaphoreReleaseOnNormalOperation()
         {
-            //System.IO.Stream fakeStream = mocks.DynamicMock<System.IO.Stream>();
-            //System.Threading.Semaphore semaphore = mocks.DynamicMock<System.Threading.Semaphore>;
+            IPAddress ipaddress = IPAddress.Parse("127.0.0.1");
+            Command command = new Command(CommandType.UserExit, ipaddress, null);
+            MemoryStream simpleStream = new MemoryStream();
+            CMDClient client = new CMDClient(null, "Bogus network name");
+            System.Threading.Semaphore fakeSemaphore = mocks.DynamicMock<System.Threading.Semaphore>();
+
+            using (mocks.Ordered())  //Must call to pass test
+            {
+                Expect.Call(fakeSemaphore.WaitOne()).Return(true);
+                Expect.Call(fakeSemaphore.Release()).Return(0);    
+            }
+            mocks.ReplayAll();
+
+            typeof(CMDClient)
+                .GetField("networkStream", BindingFlags.Instance | BindingFlags.NonPublic)
+                .SetValue(client, simpleStream);
+            typeof(CMDClient)
+                .GetField("semaphore", BindingFlags.Instance | BindingFlags.NonPublic)
+                .SetValue(client, fakeSemaphore);
+            client.SendCommandToServerUnthreaded(command);
+            byte[] actual = simpleStream.ToArray();
+            byte[] expectedArray = { 0, 0, 0, 0, 9, 0, 0, 0, 49, 50, 55, 46, 48, 46, 48, 46, 49, 2, 0, 0, 0, 10, 0 };
+            CollectionAssert.AreEqual(expectedArray, actual);
+                    
+            mocks.VerifyAll();
         }
 
         [TestMethod]
         public void TestSemaphoreReleaseOnExceptionalOperation()
         {
-            Assert.Fail("Not yet implemented");
+            IPAddress ipaddress = IPAddress.Parse("127.0.0.1");
+            Command command = new Command(CommandType.UserExit, ipaddress, null);
+            CMDClient client = new CMDClient(null, "Bogus network name");
+            System.IO.Stream simpleStream = mocks.DynamicMock<System.IO.Stream>();
+            byte[] commandBytes = { 0, 0, 0, 0 };
+            byte[] ipLength = { 9, 0, 0, 0 };
+            byte[] ip = { 49, 50, 55, 46, 48, 46, 48, 46, 49 };
+            byte[] metaDataLength = { 2, 0, 0, 0 };
+            byte[] metaData = { 10, 0 };
+            System.Threading.Semaphore fakeSemaphore = mocks.DynamicMock<System.Threading.Semaphore>();
 
+            using (mocks.Ordered())
+            {
+                Expect.Call(fakeSemaphore.WaitOne()).Return(true);
+                simpleStream.Write(commandBytes, 0, 4);
+                simpleStream.Flush();
+                simpleStream.Write(ipLength, 0, 4);
+                simpleStream.Flush();
+                simpleStream.Write(ip, 0, 9);
+                simpleStream.Flush();
+                simpleStream.Write(metaDataLength, 0, 4);
+                simpleStream.Flush();
+                simpleStream.Write(metaData, 0, 2);
+                simpleStream.Flush();
+                LastCall.On(simpleStream).Throw(new IOException());
+            }
+            mocks.ReplayAll();
+
+            typeof(CMDClient)
+                .GetField("networkStream", BindingFlags.Instance | BindingFlags.NonPublic)
+                .SetValue(client, simpleStream);
+            typeof(CMDClient)
+                .GetField("semaphore", BindingFlags.Instance | BindingFlags.NonPublic)
+                .SetValue(client, fakeSemaphore);
+            try
+            {
+                client.SendCommandToServerUnthreaded(command);
+            }
+            catch (IOException)
+            {
+                Console.Out.Write("Exception Thrown");
+            }
+            mocks.VerifyAll();
         }
     }
 }
